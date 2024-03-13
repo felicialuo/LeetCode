37f7a6b1dfcdc788e46040acfcd216a06f33fd63
Felicia Luo <129685045+felicialuo@users.noreply.github.com>
2024-03-13 11:03:15 -0400

    :ballot_box_with_check: find-the-pivot-integer [Runtime: 26 ms; Memory: 16.5 MB]

--- prefix-sum/find-the-pivot-integer.py
+++ prefix-sum/find-the-pivot-integer.py
@@ -2,6 +2,7 @@ class Solution:
     def pivotInteger(self, n: int) -> int:
         x = sqrt(n * (n + 1) / 2)
         
+        # test if github
         if x % 1 != 0:
             return -1
         else:
