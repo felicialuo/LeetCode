2e73e397940c8adb33c0bff143fab08b50f0c8e7
Felicia Luo <129685045+felicialuo@users.noreply.github.com>
2024-03-14 22:42:23 -0400

    :ballot_box_with_check: two-sum [Runtime: 57 ms; Memory: 17.9 MB]

--- hash-table/two-sum.py
+++ hash-table/two-sum.py
@@ -1,21 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # brute force
-        # for i in range(len(nums)):
-        #     for j in range(i+1, len(nums)):
-        #         if nums[i] + nums[j] == target:
-        #             return [i,j]
-        # return []
-
-        # optimized
-        num_to_idx = {}
-        # for idx, num in enumerate(nums):
-        #     num_to_idx[num] = idx
-        for i in range(len(nums)):
-            num = nums[i]
-            if target - num in num_to_idx:
-                return [i, num_to_idx[target-num]]
-            num_to_idx[num] = i
+        # always two nums
+        num2idx = {}
+        for i, num in enumerate(nums):
+            if target - num in num2idx:
+                return [i, num2idx[target - num]]
+            num2idx[num] = i
         return []
-                
+            
         
